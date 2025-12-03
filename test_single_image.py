d96227140377736d7e275a836d6d11b820b62353
Julian  Rosas <julianrosas.2003@gmail.com>
2025-12-02 18:56:25 -0600

    Add testing for a single image

---
+++ test_single_image.py
@@ -0,0 +1,13 @@
+from inference import super_resolve_image
+import os
+
+# Test on a single image
+input_image = 'gau2.png'  # Change to any image filename
+output_image = 'output/test_deblurred.png'
+
+# Make sure output directory exists
+os.makedirs('output', exist_ok=True)
+
+# Deblur the image
+super_resolve_image(input_image, output_image)
+print(f"Done! Check {output_image}")
